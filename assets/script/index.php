44ae85a0f15100f6e4b5ef87b6a1e32fd48a5a79
Onjatiana-Léoncia <leonciarasoa@gmail.com>
2023-08-20 21:31:12 +0300

    modification-js

--- assets/script/index.php
+++ assets/script/index.php
@@ -3,41 +3,58 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="./css/index.css">
     <title>Madagasikara | Lisitry ny Faritra</title>
 </head>
 <body>
+    <!-- Contenues de la page -->
     <div class="contenu">
-        <div class="titre">
-            <img  style ="Width:200px;heigt:200px;"src="./img/drapeau.jpg"  alt="logo-drapeau-rond">
-            <span  class="titre-span">LISITRY NY FARITRA REHETRA ETO MADAGASIKARA</span>
-            <img style ="Width:200px;heigt:200px;" src="./img/drapeau.jpg" alt="logo-drapeau-rond">
+        <!-- Logo -->
+        <div class="logo-container">
+            <img class="logo-drapeau" src="./img/drapeau.jpg" alt="logo-drapeau-rond">
         </div>
-
+        <div class="logo-container-2">
+            <img class="logo-drapeau" src="./img/drapeau.jpg" alt="logo-drapeau-rond">
+        </div>
+        <div class="titre">
+            <span class="titre-span">LISITRY NY FARITRA REHETRA ETO MADAGASIKARA</span>
+        </div>
         <div id="bar-recherche">
+        <!-- Barre de recherche -->
+            <div class="recherche-wrapper">
+                <span class="icon">
+                    <i class="fas fa-search"></i>
+                </span>
+                <input type="text" id="recherche-input" class="recherche-input" 
+                placeholder="Eto raha hitady faritra...">
+            </div>
+            <!-- Paginations -->
             <div class="pagination">
                 <select id="items-par-page">
-                    <option value="all">Aseho avokoa ny lisitra rehetra</option>
-                    <option value="10">10 lisitra isakin'ny pejy</option>
-                    <option value="25">25 lisitra isakin'ny pejy</option>
-                    <option value="50">55 lisitra isakin'ny pejy</option>
-                    <option value="75">75 lisitra isakin'ny pejy</option>
-                    <option value="100">100 lisitra isakin'ny pejy</option>
+                    <option value="" disabled>Safidio ny isany lisitra miseho</option>
+                    <option value="all">Aseho avokoa ireo lisitra</option>
+                    <option value="1012">1012 lisitra isakin'ny pejy</option>
+                    <option value="2024">2024 lisitra isakin'ny pejy</option>
+                    <option value="4048">4048 lisitra isakin'ny pejy</option>
+                    <option value="6096">6096 lisitra isakin'ny pejy</option>
+                    <option value="8092">8092 lisitra isakin'ny pejy</option>
                 </select>
             </div>
-            <!-- icone--><input type="text" id="recherche-input" class="recherche-input" placeholder="Soraty eto ny faritra tianao hotadiavina..."> 
         </div>
-    <div id="liste-faritra">
-        <!-- PS: Toutes les FARITRA seront affichés ici -->
-    </div>
-    
-    <div id="prev-next">
-        <button id="prev-button">Aloha</button>
-        <button id="next-button">Aoriana</button>
-    </div>
+        
+        <div id="liste-faritra">
+           <!-- Données -->
+        </div>
+        <!-- Liens preview et next -->
+        <div id="prev-next">
+            <button id="prev-button">Aloha</button>
+            <div id="page-links"></div>
+            <button id="next-button">Aoriana</button>
+        </div>
     </div>
 
-    <script>
+    <!-- <script>
         fetch('read-data.php')
         .then((response)=>response.json())
         .then((data)=>{
@@ -49,7 +66,8 @@
       // miatomboka 1 ilay i dia tsy miseo ilat soratra fkt sy tariny anaty ilay ficher
        for (var i = 1; i < 20; i++) {
          for (var j = 5; j < data[i].length; j++) {
-            
+                    faritraItem.className = "liste-faritra"; // Ajout de la classe
+
                     faritraItem.innerHTML += `
                     <div>
                         <strong>N°${data[i][0]}</strong><br>
@@ -61,11 +79,12 @@
                     </div>
                     `;
                     console.log( data[i][j]);
+                    
                 }
            
             }
 })      
-</script>
+</script> -->
     <script src="script.js"></script>
 </body>
 </html>
--- assets/script/script.js
+++ assets/script/script.js
@@ -1,78 +1,18 @@
 console.log("poinsa");
 
-
-// const faritraList = [
-//     // Listes Faritra en Json ici
-//     { N: "1", fokontany: "Ampasina", kaomina: "Andina", distrika: "Ambositra", region: "Amoron'i Mania", province: "Fianarantsoa"},
-//     { N: "2", fokontany: "Amboloando", kaomina: "Andiolava", distrika: "Ambositra", region: "Amoron'i Mania", province: "Fianarantsoa"},
-//     { N: "3", fokontany: "Antanifotsy", kaomina: "Ambositra", distrika: "Ambositra", region: "Amoron'i Mania", province: "Fianarantsoa"},
-//     { N: "4", fokontany: "Ampasina", kaomina: "Andina", distrika: "Ambositra", region: "Amoron'i Mania", province: "Fianarantsoa"},
-//     { N: "5", fokontany: "Amboloando", kaomina: "Andiolava", distrika: "Ambositra", region: "Amoron'i Mania", province: "Fianarantsoa"},
-//     { N: "6", fokontany: "Antanifotsy", kaomina: "Ambositra", distrika: "Ambositra", region: "Amoron'i Mania", province: "Fianarantsoa"},
-//     { N: "7", fokontany: "Ampasina", kaomina: "Andina", distrika: "Ambositra", region: "Amoron'i Mania", province: "Fianarantsoa"},
-//     { N: "9", fokontany: "Amboloando", kaomina: "Andiolava", distrika: "Ambositra", region: "Amoron'i Mania", province: "Fianarantsoa"},
-//     { N: "10", fokontany: "Antanifotsy", kaomina: "Ambositra", distrika: "Ambositra", region: "Amoron'i Mania", province: "Fianarantsoa"},
-
-//     { N: "1", fokontany: "Ampasina", kaomina: "Andina", distrika: "Ambositra", region: "Amoron'i Mania", province: "Fianarantsoa"},
-//     { N: "2", fokontany: "Amboloando", kaomina: "Andiolava", distrika: "Ambositra", region: "Amoron'i Mania", province: "Fianarantsoa"},
-//     { N: "3", fokontany: "Antanifotsy", kaomina: "Ambositra", distrika: "Ambositra", region: "Amoron'i Mania", province: "Fianarantsoa"},
-//     { N: "4", fokontany: "Ampasina", kaomina: "Andina", distrika: "Ambositra", region: "Amoron'i Mania", province: "Fianarantsoa"},
-//     { N: "5", fokontany: "Amboloando", kaomina: "Andiolava", distrika: "Ambositra", region: "Amoron'i Mania", province: "Fianarantsoa"},
-//     { N: "6", fokontany: "Antanifotsy", kaomina: "Ambositra", distrika: "Ambositra", region: "Amoron'i Mania", province: "Fianarantsoa"},
-//     { N: "7", fokontany: "Ampasina", kaomina: "Andina", distrika: "Ambositra", region: "Amoron'i Mania", province: "Fianarantsoa"},
-//     { N: "9", fokontany: "Amboloando", kaomina: "Andiolava", distrika: "Ambositra", region: "Amoron'i Mania", province: "Fianarantsoa"},
-//     { N: "10", fokontany: "Antanifotsy", kaomina: "Ambositra", distrika: "Ambositra", region: "Amoron'i Mania", province: "Fianarantsoa"},
-
-//     { N: "1", fokontany: "Ampasina", kaomina: "Andina", distrika: "Ambositra", region: "Amoron'i Mania", province: "Fianarantsoa"},
-//     { N: "2", fokontany: "Amboloando", kaomina: "Andiolava", distrika: "Ambositra", region: "Amoron'i Mania", province: "Fianarantsoa"},
-//     { N: "3", fokontany: "Antanifotsy", kaomina: "Ambositra", distrika: "Ambositra", region: "Amoron'i Mania", province: "Fianarantsoa"},
-//     { N: "4", fokontany: "Ampasina", kaomina: "Andina", distrika: "Ambositra", region: "Amoron'i Mania", province: "Fianarantsoa"},
-//     { N: "5", fokontany: "Amboloando", kaomina: "Andiolava", distrika: "Ambositra", region: "Amoron'i Mania", province: "Fianarantsoa"},
-//     { N: "6", fokontany: "Antanifotsy", kaomina: "Ambositra", distrika: "Ambositra", region: "Amoron'i Mania", province: "Fianarantsoa"},
-//     { N: "7", fokontany: "Ampasina", kaomina: "Andina", distrika: "Ambositra", region: "Amoron'i Mania", province: "Fianarantsoa"},
-//     { N: "9", fokontany: "Amboloando", kaomina: "Andiolava", distrika: "Ambositra", region: "Amoron'i Mania", province: "Fianarantsoa"},
-//     { N: "10", fokontany: "Antanifotsy", kaomina: "Ambositra", distrika: "Ambositra", region: "Amoron'i Mania", province: "Fianarantsoa"},
-
-//     { N: "1", fokontany: "Ampasina", kaomina: "Andina", distrika: "Ambositra", region: "Amoron'i Mania", province: "Fianarantsoa"},
-//     { N: "2", fokontany: "Amboloando", kaomina: "Andiolava", distrika: "Ambositra", region: "Amoron'i Mania", province: "Fianarantsoa"},
-//     { N: "3", fokontany: "Antanifotsy", kaomina: "Ambositra", distrika: "Ambositra", region: "Amoron'i Mania", province: "Fianarantsoa"},
-//     { N: "4", fokontany: "Ampasina", kaomina: "Andina", distrika: "Ambositra", region: "Amoron'i Mania", province: "Fianarantsoa"},
-//     { N: "5", fokontany: "Amboloando", kaomina: "Andiolava", distrika: "Ambositra", region: "Amoron'i Mania", province: "Fianarantsoa"},
-//     { N: "6", fokontany: "Antanifotsy", kaomina: "Ambositra", distrika: "Ambositra", region: "Amoron'i Mania", province: "Fianarantsoa"},
-//     { N: "7", fokontany: "Ampasina", kaomina: "Andina", distrika: "Ambositra", region: "Amoron'i Mania", province: "Fianarantsoa"},
-//     { N: "9", fokontany: "Amboloando", kaomina: "Andiolava", distrika: "Ambositra", region: "Amoron'i Mania", province: "Fianarantsoa"},
-//     { N: "10", fokontany: "Antanifotsy", kaomina: "Ambositra", distrika: "Ambositra", region: "Amoron'i Mania", province: "Fianarantsoa"},
-
-//     { N: "1", fokontany: "Ampasina", kaomina: "Andina", distrika: "Ambositra", region: "Amoron'i Mania", province: "Fianarantsoa"},
-//     { N: "2", fokontany: "Amboloando", kaomina: "Andiolava", distrika: "Ambositra", region: "Amoron'i Mania", province: "Fianarantsoa"},
-//     { N: "3", fokontany: "Antanifotsy", kaomina: "Ambositra", distrika: "Ambositra", region: "Amoron'i Mania", province: "Fianarantsoa"},
-//     { N: "4", fokontany: "Ampasina", kaomina: "Andina", distrika: "Ambositra", region: "Amoron'i Mania", province: "Fianarantsoa"},
-//     { N: "5", fokontany: "Amboloando", kaomina: "Andiolava", distrika: "Ambositra", region: "Amoron'i Mania", province: "Fianarantsoa"},
-//     { N: "6", fokontany: "Antanifotsy", kaomina: "Ambositra", distrika: "Ambositra", region: "Amoron'i Mania", province: "Fianarantsoa"},
-//     { N: "7", fokontany: "Ampasina", kaomina: "Andina", distrika: "Ambositra", region: "Amoron'i Mania", province: "Fianarantsoa"},
-//     { N: "9", fokontany: "Amboloando", kaomina: "Andiolava", distrika: "Ambositra", region: "Amoron'i Mania", province: "Fianarantsoa"},
-//     { N: "10", fokontany: "Antanifotsy", kaomina: "Ambositra", distrika: "Ambositra", region: "Amoron'i Mania", province: "Fianarantsoa"},
-
-//     { N: "1", fokontany: "Ampasina", kaomina: "Andina", distrika: "Ambositra", region: "Amoron'i Mania", province: "Fianarantsoa"},
-//     { N: "2", fokontany: "Amboloando", kaomina: "Andiolava", distrika: "Ambositra", region: "Amoron'i Mania", province: "Fianarantsoa"},
-//     { N: "3", fokontany: "Antanifotsy", kaomina: "Ambositra", distrika: "Ambositra", region: "Amoron'i Mania", province: "Fianarantsoa"},
-//     { N: "4", fokontany: "Ampasina", kaomina: "Andina", distrika: "Ambositra", region: "Amoron'i Mania", province: "Fianarantsoa"},
-//     { N: "5", fokontany: "Amboloando", kaomina: "Andiolava", distrika: "Ambositra", region: "Amoron'i Mania", province: "Fianarantsoa"},
-//     { N: "6", fokontany: "Antanifotsy", kaomina: "Ambositra", distrika: "Ambositra", region: "Amoron'i Mania", province: "Fianarantsoa"},
-//     { N: "7", fokontany: "Ampasina", kaomina: "Andina", distrika: "Ambositra", region: "Amoron'i Mania", province: "Fianarantsoa"},
-//     { N: "9", fokontany: "Amboloando", kaomina: "Andiolava", distrika: "Ambositra", region: "Amoron'i Mania", province: "Fianarantsoa"},
-//     { N: "10", fokontany: "Antanifotsy", kaomina: "Ambositra", distrika: "Ambositra", region: "Amoron'i Mania", province: "Fianarantsoa"},
-  
-// ];
-
+// Récupération des éléments du DOM
 const rechercheInput = document.getElementById("recherche-input");
 const listeFaritraContenues = document.getElementById("liste-faritra");
 const prevButton = document.getElementById("prev-button");
 const nextButton = document.getElementById("next-button");
 const itemsParPageSelect = document.getElementById("items-par-page");
+const pageLinksContainer = document.getElementById("page-links");
 let currentPage = 1;
 let elementsParPage = parseInt(itemsParPageSelect.value);
-console.log("poinsa");
+let faritraList = [];
+let filteredFaritras = [];
+
+// Fonction pour afficher les Faritra dans le DOM
 function displayFaritra(faritras) {
     listeFaritraContenues.innerHTML = "";
 
@@ -81,66 +21,119 @@ function displayFaritra(faritras) {
     const faritrasToDisplay = faritras.slice(startIndex, endIndex);
 
     faritrasToDisplay.forEach(faritra => {
-        const faritraItem = document.createElement("li");
+        const faritraItem = document.createElement("div");
+        faritraItem.className = "liste-faritra";
         faritraItem.innerHTML = `
-            <strong>${faritra.N}</strong> - ${faritra.fokontany} - ${faritra.kaomina}- ${faritra.distrika}- ${faritra.region}- ${faritra.province}
+            <strong>N°</strong><strong>${faritra[0]}</strong><br>
+            <strong>Fokontany :</strong> ${faritra[1]}<br>
+            <strong>Kaomina :</strong> ${faritra[2]}<br>
+            <strong>Distrika :</strong> ${faritra[3]}<br>
+            <strong>Region :</strong> ${faritra[4]}<br>
+            <strong>Province :</strong> ${faritra[5]}<br>
         `;
         listeFaritraContenues.appendChild(faritraItem);
     });
 }
 
+// Fonction pour filtrer et afficher les Faritra en fonction de la recherche
 function performRecherche(query) {
-    const filteredFaritras = faritraList.filter(faritra => {
-        return (faritra.fokontany.toLowerCase().includes(query.toLowerCase()) ||
-                faritra.kaomina.toLowerCase().includes(query.toLowerCase()) ||
-                faritra.distrika.toLowerCase().includes(query.toLowerCase()) ||
-                faritra.region.toLowerCase().includes(query.toLowerCase()) ||
-                faritra.province.toLowerCase().includes(query.toLowerCase()) );
+    filteredFaritras = faritraList.filter(faritra => {
+        return (
+            faritra[0].toLowerCase().includes(query.toLowerCase()) ||
+            faritra[1].toLowerCase().includes(query.toLowerCase()) ||
+            faritra[2].toLowerCase().includes(query.toLowerCase()) ||
+            faritra[3].toLowerCase().includes(query.toLowerCase()) ||
+            faritra[4].toLowerCase().includes(query.toLowerCase()) ||
+            faritra[5].toLowerCase().includes(query.toLowerCase())
+        );
     });
 
+    currentPage = 1;
     displayFaritra(filteredFaritras);
-    updatePagination(filteredFaritras.length);
+    updatePagination();
 }
 
-function updatePagination(totalItems) {
-    const totalPages = Math.ceil(totalItems / elementsParPage);
+// Fonction pour mettre à jour les éléments de pagination
+function updatePagination() {
+    const totalPages = Math.ceil(filteredFaritras.length / elementsParPage);
 
     prevButton.disabled = currentPage === 1;
     nextButton.disabled = currentPage === totalPages;
 
-    prevButton.addEventListener("click", () => {
-        if (currentPage > 1) {
-            currentPage--;
-            performRecherche(rechercheInput.value);
-        }
-    });
+    pageLinksContainer.innerHTML = "";
 
-    nextButton.addEventListener("click", () => {
-        if (currentPage < totalPages) {
-            currentPage++;
-            performRecherche(rechercheInput.value);
+    for (let i = 1; i <= totalPages; i++) {
+        const pageLink = document.createElement("a");
+        pageLink.textContent = i;
+        pageLink.href = "#";
+        pageLink.dataset.page = i;
+        pageLinksContainer.appendChild(pageLink);
+
+        if (i === currentPage) {
+            pageLink.classList.add("active");
         }
+    }
+
+    const pageLinks = pageLinksContainer.querySelectorAll("a");
+
+    pageLinks.forEach(link => {
+        link.addEventListener("click", event => {
+            event.preventDefault();
+            const requestedPage = parseInt(link.dataset.page);
+
+            if (requestedPage >= 1 && requestedPage <= totalPages) {
+                currentPage = requestedPage;
+                displayFaritra(filteredFaritras);
+                updatePagination();
+            }
+        });
     });
 }
 
+// Gestionnaire d'événement pour le changement du nombre d'éléments par page
 itemsParPageSelect.addEventListener("change", function () {
     const selectedValue = itemsParPageSelect.value;
     if (selectedValue === "all") {
         elementsParPage = faritraList.length;
-        currentPage = 1; 
-        performRecherche(rechercheInput.value);
     } else {
         elementsParPage = parseInt(selectedValue);
-        currentPage = 1;  
-        performRecherche(rechercheInput.value);
+    }
+
+    currentPage = 1;
+    displayFaritra(filteredFaritras);
+    updatePagination();
+});
+
+// Gestionnaire d'événement pour la recherche en temps réel
+rechercheInput.addEventListener("input", function () {
+    const query = rechercheInput.value;
+    performRecherche(query);
+});
+
+// Gestionnaire d'événement pour le bouton "Aloha" (Précédent)
+prevButton.addEventListener("click", () => {
+    if (currentPage > 1) {
+        currentPage--;
+        displayFaritra(filteredFaritras);
+        updatePagination();
     }
 });
 
-rechercheInput.addEventListener("input", function () {
-    const query = rechercheInput.value;
-    performSearch(query);
+// Gestionnaire d'événement pour le bouton "Aoriana" (Suivant)
+nextButton.addEventListener("click", () => {
+    const totalPages = Math.ceil(filteredFaritras.length / elementsParPage);
+    if (currentPage < totalPages) {
+        currentPage++;
+        displayFaritra(filteredFaritras);
+        updatePagination();
+    }
 });
 
-// performSearch(""); // Afficher tous les faritra au chargement initial
-
-// JE COMMENTERAI PETIT A PETIT ICI
+// Charge initiale des données
+fetch('read-data.php')
+    .then(response => response.json())
+    .then(data => {
+        faritraList = data;
+        filteredFaritras = faritraList;
+        performRecherche("");
+    });
